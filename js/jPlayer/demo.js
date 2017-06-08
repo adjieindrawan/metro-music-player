6a999050defd82c54444b0f350073a7d15ddb402
indrawanadjie <adjieindrawan@gmail.com>
2017-06-09 06:55:47 +0700

    add side, song, display view, and update some css

--- js/jPlayer/demo.js
+++ js/jPlayer/demo.js
@@ -7,49 +7,49 @@ $(document).ready(function(){
     {
       title:"Busted Chump",
       artist:"ADG3",
-      mp3:"http://flatfull.com/themes/assets/musics/adg3com_bustedchump.mp3",
+      mp3:"The_Moose_-_We_Got_The_Love.mp3",
       poster: "images/m0.jpg"
     },
     {
       title:"Chucked Knuckles",
       artist:"3studios",
-      mp3:"http://flatfull.com/themes/assets/musics/adg3com_chuckedknuckles.mp3",
+      mp3:"The_Moose_-_We_Got_The_Love.mp3",
       poster: "images/m0.jpg"
     },
     {
       title:"Cloudless Days",
       artist:"ADG3 Studios",
-      mp3:"http://flatfull.com/themes/assets/musics/adg3com_cloudlessdays.mp3",
+      mp3:"The_Moose_-_We_Got_The_Love.mp3",
       poster: "images/m0.jpg"
     },
     {
       title:"Core Issues",
       artist:"Studios",
-      mp3:"http://flatfull.com/themes/assets/musics/adg3com_coreissues.mp3",
+      mp3:"The_Moose_-_We_Got_The_Love.mp3",
       poster: "images/m0.jpg"
     },
     {
       title:"Cryptic Psyche",
       artist:"ADG3",
-      mp3:"http://flatfull.com/themes/assets/musics/adg3com_crypticpsyche.mp3",
+      mp3:"The_Moose_-_We_Got_The_Love.mp3",
       poster: "images/m0.jpg"
     },
     {
       title:"Electro Freak",
       artist:"Studios",
-      mp3:"http://flatfull.com/themes/assets/musics/adg3com_electrofreak.mp3",
+      mp3:"The_Moose_-_We_Got_The_Love.mp3",
       poster: "images/m0.jpg"
     },
     {
       title:"Freeform",
       artist:"ADG",
-      mp3:"http://flatfull.com/themes/assets/musics/adg3com_freeform.mp3",
+      mp3:"The_Moose_-_We_Got_The_Love.mp3",
       poster: "images/m0.jpg"
     }
   ], {
     playlistOptions: {
       enableRemoveControls: true,
-      autoPlay: true
+      autoPlay: false
     },
     swfPath: "js/jPlayer",
     supplied: "webmv, ogv, m4v, oga, mp3",
