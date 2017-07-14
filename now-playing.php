0960620b11e53ad8a6091267fde6dfcd6b896576
indrawanadjie <adjieindrawan@gmail.com>
2017-07-14 07:50:32 +0700

    rebase svn rev 14

---
+++ now-playing.php
@@ -0,0 +1,18 @@
+<?php require_once('header.php'); ?>
+<?php require_once('aside.php'); ?>
+<section id="content">
+    <section class="hbox stretch">
+        <section>
+            <section class="vbox">
+                <section class="scrollable padder-lg bg-black p-b" id="bjax-target">
+                	<div class="container-fluid">
+	                    <div class="row">
+		                    <div class="col-md-12 img-responsive" id="video-container">
+								<video class="video-play responsive-video" autoplay preload="auto"> 	
+								  <source src=http://www.sample-videos.com/video/mp4/720/big_buck_bunny_720p_5mb.mp4 type=video/mp4>	
+								</video>
+		                    </div>
+	                    </div>                		
+                	</div>
+                </section>
+                <?php require_once('footer.php'); ?>
